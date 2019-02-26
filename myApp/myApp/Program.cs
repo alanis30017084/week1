5409c8417bcd0c09c688be04b30ef6f06b28618f
Alanis Correa Kriener <30017084@student.toiohomai.ac.nz>
2019-02-27 10:44:59 +1300

    Name and age app

---
+++ myApp/myApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}, nice to meet you.", name);
+            Console.ReadLine();
+            */
+
+            // Interger variable
+            int age;
+            string name;
+
+
+            Console.Write("Please enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("We have recorded your age as {0} and name as {1}.", age, name);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
